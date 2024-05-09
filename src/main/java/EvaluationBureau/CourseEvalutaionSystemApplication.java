c7a16e63cd3c39c8acc8cb66e94b4a5e1853f5af
Mdsoln <muddyfakih98@gmail.com>
2024-05-09 22:33:02 +0300

    initial configurations

---
+++ src/main/java/EvaluationBureau/CourseEvalutaionSystemApplication.java
@@ -0,0 +1,13 @@
+package EvaluationBureau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEvalutaionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEvalutaionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EvaluationBureau/CourseEvalutaionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package EvaluationBureau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEvalutaionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
